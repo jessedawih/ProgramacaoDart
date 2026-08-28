f330267dadd7ce443c7c2abca0dcf727e82219b9
Jesse Chinonye Okolo <63160949+jessedawih@users.noreply.github.com>
2026-08-28 20:41:07 -0300

    Create 04-tipos-explicitos.dart

---
+++ 04-tipos-explicitos.dart
@@ -0,0 +1,10 @@
+void main() {
+    
+        String nome = "Jesse";
+        int idade = 29;
+        double altura = 1.82;
+        bool aprovado = true;
+  
+  
+      print( '$nome | $idade | $altura | $aprovado');
+  }
