827115df21b731b3ed23a017024494ca7731607e
Jesse Chinonye Okolo <63160949+jessedawih@users.noreply.github.com>
2026-08-28 21:29:48 -0300

    Create 11-else-if.dart

---
+++ 11-else-if.dart
@@ -0,0 +1,18 @@
+void main() {
+    
+ 
+    
+  int idade = 20;
+   if (idade < 16) {
+      print("Não vota");
+     }else if(idade < 18 || idade >= 70 ) {
+     
+        print("voto facultativo");
+     }else{
+     
+        print("voto é obrigatorio"); 
+   }
+  
+  
+  }
+
