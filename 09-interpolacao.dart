a4b39d901051d733c0b7b207e1d397f88d9c2fad
Jesse Chinonye Okolo <63160949+jessedawih@users.noreply.github.com>
2026-08-28 21:19:59 -0300

    Create 09-interpolacao.dart

---
+++ 09-interpolacao.dart
@@ -0,0 +1,11 @@
+void main() {
+    
+        String nome = "Ariel";
+        double nota = 10;
+     
+       print ('Aluno : $nome');
+       print ('Nota final ; ${nota.toStringAsFixed(1)}');
+  
+  
+  }
+
