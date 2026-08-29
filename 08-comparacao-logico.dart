da7aafd24031a1453b6a55ae5de8af4870fd1c73
Jesse Chinonye Okolo <63160949+jessedawih@users.noreply.github.com>
2026-08-28 21:13:13 -0300

    Create 08-comparacao-logico.dart

---
+++ 08-comparacao-logico.dart
@@ -0,0 +1,12 @@
+void main() {
+    
+         int idade = 20;
+         bool possuiDocmento = true;
+  
+        print(idade > 18);
+        print(idade >= 18 && possuiDocmento);
+        print(idade < 18 || !possuiDocmento);
+  
+  
+  }
+
